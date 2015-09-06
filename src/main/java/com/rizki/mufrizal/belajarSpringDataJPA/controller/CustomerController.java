460c04ac2b547264cd143bd8a30e5a40d266793e
RizkiMufrizal <mufrizalrizki@gmail.com>
2015-09-06 19:09:59 +0700

    Update customer controller

--- src/main/java/com/rizki/mufrizal/belajarSpringDataJPA/controller/CustomerController.java
+++ src/main/java/com/rizki/mufrizal/belajarSpringDataJPA/controller/CustomerController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+
 /**
  * Created by rizki on 06/09/15.
  */
@@ -23,8 +25,54 @@ public class CustomerController {
     @RequestMapping(value = "/customer", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
     public Page<Customer> getCustomers(
             @RequestParam(value = "page") Integer page,
-            @RequestParam(value = "jumlah") Integer jumlah){
+            @RequestParam(value = "jumlah") Integer jumlah) {
         return customerService.findAllCustomer(page, jumlah);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "/customer/{idCustomer}", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public Customer getCustomer(@PathVariable("idCustomer") String idCustomer) {
+        return customerService.getCustomer(idCustomer);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(value = "/customer", method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_VALUE}, consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public HashMap<String, Object> saveCustomer(@RequestBody Customer customer) {
+
+        HashMap<String, Object> objectHashMap = new HashMap<>();
+
+        if (customer == null) {
+            objectHashMap.put("Success", Boolean.FALSE);
+            objectHashMap.put("Info", "Data tidak ada yang terkirim");
+            return objectHashMap;
+        } else {
+            customerService.saveCustomer(customer);
+        }
+
+        objectHashMap.put("Success", Boolean.TRUE);
+        objectHashMap.put("Info", "Data berhasil disimpan");
+        return objectHashMap;
+
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "/customer", method = RequestMethod.PUT, produces = {MediaType.APPLICATION_JSON_VALUE}, consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public HashMap<String, Object> updateCustomer(@RequestBody Customer customer) {
+
+        HashMap<String, Object> objectHashMap = new HashMap<>();
+
+        if (customer == null) {
+            objectHashMap.put("Success", Boolean.FALSE);
+            objectHashMap.put("Info", "Data tidak ada yang terkirim");
+            return objectHashMap;
+        } else {
+            customerService.updateCustomer(customer);
+        }
+
+        objectHashMap.put("Success", Boolean.TRUE);
+        objectHashMap.put("Info", "Data berhasil diedit");
+        return objectHashMap;
+
     }
 
 }
