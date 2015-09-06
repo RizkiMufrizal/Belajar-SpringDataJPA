7120ea21726e27195244158d9a7e1d52313d16ba
RizkiMufrizal <mufrizalrizki@gmail.com>
2015-09-06 18:51:07 +0700

    add pembelian service

---
+++ src/main/java/com/rizki/mufrizal/belajarSpringDataJPA/service/PembelianService.java
@@ -0,0 +1,14 @@
+package com.rizki.mufrizal.belajarSpringDataJPA.service;
+
+import com.rizki.mufrizal.belajarSpringDataJPA.domain.Pembelian;
+import org.springframework.data.domain.Page;
+
+/**
+ * Created by rizki on 06/09/15.
+ */
+public interface PembelianService {
+    void savePembelian(Pembelian pembelian);
+    void deletePembelian(String idPembelian);
+    Pembelian getPembelian(String idPembelian);
+    Page<Pembelian> findAllPembelian(Integer page, Integer jumlah);
+}
