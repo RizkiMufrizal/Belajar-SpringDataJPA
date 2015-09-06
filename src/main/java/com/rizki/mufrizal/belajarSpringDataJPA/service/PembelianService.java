10aee721a0d5c7b3087ebb2e0c4da999c85c4b8e
RizkiMufrizal <mufrizalrizki@gmail.com>
2015-09-06 19:43:59 +0700

    Update pembelian service

--- src/main/java/com/rizki/mufrizal/belajarSpringDataJPA/service/PembelianService.java
+++ src/main/java/com/rizki/mufrizal/belajarSpringDataJPA/service/PembelianService.java
@@ -7,7 +7,7 @@ import org.springframework.data.domain.Page;
  * Created by rizki on 06/09/15.
  */
 public interface PembelianService {
-    void savePembelian(Pembelian pembelian);
+    void savePembelian(Pembelian pembelian, String idCustomer, String idBarang);
     void deletePembelian(String idPembelian);
     Pembelian getPembelian(String idPembelian);
     Page<Pembelian> findAllPembelian(Integer page, Integer jumlah);
