ba18719c8b9819b08bb1946dbbd0785855fd66ec
RizkiMufrizal <mufrizalrizki@gmail.com>
2015-09-07 20:54:09 +0700

    add query delete

--- src/main/java/com/rizki/mufrizal/belajarSpringDataJPA/repository/BarangRepository.java
+++ src/main/java/com/rizki/mufrizal/belajarSpringDataJPA/repository/BarangRepository.java
@@ -1,6 +1,8 @@
 package com.rizki.mufrizal.belajarSpringDataJPA.repository;
 
 import com.rizki.mufrizal.belajarSpringDataJPA.domain.Barang;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +12,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BarangRepository extends PagingAndSortingRepository<Barang, String> {
+
+    @Modifying
+    @Query("delete from Barang b where b.idBarang = ?1")
+    void deleteBarang(String idBarang);
 
 }
--- src/main/java/com/rizki/mufrizal/belajarSpringDataJPA/repository/PembelianRepository.java
+++ src/main/java/com/rizki/mufrizal/belajarSpringDataJPA/repository/PembelianRepository.java
@@ -1,6 +1,8 @@
 package com.rizki.mufrizal.belajarSpringDataJPA.repository;
 
 import com.rizki.mufrizal.belajarSpringDataJPA.domain.Pembelian;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +12,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PembelianRepository extends PagingAndSortingRepository<Pembelian, String> {
+
+    @Modifying
+    @Query("delete from Pembelian p where p.idPembelian = ?1")
+    void deletePembelian(String idPembelian);
 
 }
