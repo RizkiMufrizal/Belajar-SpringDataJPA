8d4b996385744bb902b8159f1e38dd017413685d
RizkiMufrizal <mufrizalrizki@gmail.com>
2015-09-07 20:56:30 +0700

    Update app router angular

--- src/main/webapp/app/scripts/app.js
+++ src/main/webapp/app/scripts/app.js
@@ -1,3 +1,24 @@
-/**
- * Created by rizki on 06/09/15.
- */
+'use strict';
+
+angular
+  .module('Belajar', [
+    'ngRoute',
+    'ngResource'
+  ])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/views/main.html'
+      })
+      .when('/barang', {
+        templateUrl: 'app/views/barang.html',
+        controller: 'BarangController'
+      })
+      .when('/pembelian', {
+        templateUrl: 'app/views/pembelian.html',
+        controller: 'PembelianController'
+      })
+      .when('/about', {
+        templateUrl: 'app/views/about.html'
+      });
+  }]);
